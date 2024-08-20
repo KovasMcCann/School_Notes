10e25a87421391967c5daaf594ce40ce40885200
Kovas McCann <Kovasmccann@outlook.com>
2024-08-20 13:01:11 -0500

    Lesson_1

--- Lesson_1/Output.java
+++ Lesson_1/Output.java
@@ -3,15 +3,15 @@
  * This program prints out a message to the console.
  */
 
-class U1_L1_template
+class U1_L1_template // The class veriable 
 {
 
 
      public static void main(String[] args) 
      {
 
+         System.out.println("*@)#@"); // ** println is like adding \n **
          System.out.print("Hello World"); 
-         
 
      }
 

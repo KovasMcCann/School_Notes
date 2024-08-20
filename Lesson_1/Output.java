9c4f37f338f51fa81521e1a4ea872983c1b3df4b
Kovas McCann <Kovasmccann@outlook.com>
2024-08-20 18:35:41 +0000

    Lesson_1 complete

--- Lesson_1/Output.java
+++ Lesson_1/Output.java
@@ -8,10 +8,13 @@ class Output // The class veriable
 
 
      public static void main(String[] args) 
-     {
+     { 
 
          System.out.println("*@)#@"); // ** println is like adding \n **
-         System.out.print("Hello World"); 
+         System.out.print("Hello World"); // you need ; at the end of the line
+
+         //Math in java
+         System.out.println("\n" + (5+5)); // \n is for clean output
 
      }
 

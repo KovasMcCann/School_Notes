201a1dab4a90eebb83c06df31667b1ccdbfd8c65
Kovas McCann <Kovasmccann@outlook.com>
2024-08-20 12:55:14 -0500

    Lesson 1

---
+++ Lesson_1/Output.java
@@ -0,0 +1,18 @@
+/*
+ * Unit 1 - Lesson 1 - Output in Java
+ * This program prints out a message to the console.
+ */
+
+class U1_L1_template
+{
+
+
+     public static void main(String[] args) 
+     {
+
+         System.out.print("Hello World"); 
+         
+
+     }
+
+}

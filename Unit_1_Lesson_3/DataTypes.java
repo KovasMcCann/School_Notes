64f81362d09c5336c654515e8da7e9acc89ce155
Kovas McCann <Kovasmccann@outlook.com>
2024-08-23 18:24:49 +0000

    Unit_1_Lesson_3 completed

--- Unit_1_Lesson_3/DataTypes.java
+++ Unit_1_Lesson_3/DataTypes.java
@@ -1,3 +1,40 @@
 public class DataTypes {
-
+// Code not needed basic examples only
+  public static void main(String[] args) {
+    // Primitive Data Types
+    // byte
+    byte myByte = 127;
+    System.out.println(myByte);
+    // short
+    short myShort = 32767;
+    System.out.println(myShort);
+    // int
+    int myInt = 2147483647;
+    System.out.println(myInt);
+    // long
+    long myLong = 9223372036854775807L;
+    System.out.println(myLong);
+    // float
+    float myFloat = 3.4028235E38f;
+    System.out.println(myFloat);
+    // double
+    double myDouble = 1.7976931348623157E308;
+    System.out.println(myDouble);
+    // char
+    char myChar = 'A';
+    System.out.println(myChar);
+    // boolean
+    boolean myBoolean = true;
+    System.out.println(myBoolean);
+    // Reference Data Types
+    // String
+    String myString = "Hello World!";
+    System.out.println(myString);
+    // Array
+    int[] myArray = {1, 2, 3, 4, 5};
+    System.out.println(myArray[0]);
+    // Class
+    // MyClass myClass = new MyClass();
+    // System.out.println(myClass);
+  }
 }
